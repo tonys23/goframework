a2967234b597d3bff05932b49e501685cf4efdd0
Patrick Romão <patrick.hernandez.vendor@europ-assistance.com.br>
2024-02-07 22:42:11 -0300

    finished

--- kafka_producer.go
+++ kafka_producer.go
@@ -92,6 +92,74 @@ func NewKafkaProducer[T interface{}](k *GoKafka,
 	}
 }
 
+func (kp *KafkaProducer[T]) PublishWithKey(ctx context.Context, key []byte, msgs ...*T) error {
+	txn := newrelic.FromContext(ctx)
+	nrSegment := txn.StartSegment(kp.kcs.Topic)
+	nrSegment.AddAttribute("span.kind", "client")
+	defer nrSegment.End()
+
+	headers := helperContextKafka(ctx,
+		map[string]string{
+			"X-Tenant-Id":      "X-Tenant-Id",
+			"X-Author":         "X-Author",
+			"X-Author-Id":      "X-Author-Id",
+			"X-Correlation-Id": "X-Correlation-Id"})
+
+	p, err := kafka.NewProducer(kp.kcm)
+	if err != nil {
+		return err
+	}
+
+	for _, m := range msgs {
+
+		hasCorrelationID := false
+		for _, v := range headers {
+			if v.Key == "X-Correlation-Id" && len(v.Value) > 0 {
+				hasCorrelationID = true
+			}
+		}
+		if !hasCorrelationID {
+			headers = append(headers, kafka.Header{Key: "X-Correlation-Id", Value: []byte(uuid.NewString())})
+		}
+
+		data, err := json.Marshal(m)
+		if err != nil {
+			return err
+		}
+
+		delivery_chan := make(chan kafka.Event, 10000)
+		if err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic:     &kp.kcs.Topic,
+				Partition: kafka.PartitionAny,
+				Offset:    kp.kcs.Offset,
+			},
+			Key:     key,
+			Value:   data,
+			Headers: headers}, delivery_chan); err != nil {
+			return err
+		}
+
+		go func() {
+			for e := range p.Events() {
+				switch ev := e.(type) {
+				case *kafka.Message:
+					if ev.TopicPartition.Error != nil {
+						log.Fatalf("Failed to deliver message: %v\n", ev.TopicPartition)
+					} else {
+						log.Printf("Successfully produced record to topic %s partition [%d] @ offset %v\n",
+							*ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
+					}
+				}
+			}
+			defer p.Close()
+		}()
+
+	}
+
+	return nil
+}
+
 func (kp *KafkaProducer[T]) Publish(ctx context.Context, msgs ...*T) error {
 
 	txn := newrelic.FromContext(ctx)
